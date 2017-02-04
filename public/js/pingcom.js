b36dec380f6f5da9b3b0037ff880b14a2f79b475
rhamill1 <rhamill11@gmail.com>
2017-02-03 17:06:36 -0800

    updates formatting for heroku scheduler

--- public/js/pingcom.js
+++ public/js/pingcom.js
@@ -1,40 +1,24 @@
 
-var CronJob = require('cron').CronJob;
-var job = new CronJob('1,31 * * * *', function() {
-// var job = new CronJob('1,31 6-23 * * *', function() {
+var request = require('request');
+var urls = new Array(
+  'http://www.ryanhamill.com',
+  'https://farmersmarketipsum.herokuapp.com/',
+  'https://amzpro.herokuapp.com',
+  'http://womensmarchtweets.herokuapp.com/'
+);
 
-  var request = require('request');
-  var urls = new Array(
-    'http://www.ryanhamill.com',
-    'https://farmersmarketipsum.herokuapp.com/',
-    'https://amzpro.herokuapp.com',
-    'http://womensmarchtweets.herokuapp.com/'
-  );
+function scrape(url){
+  request(url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log('request url: ' + url);
+      console.log('response status: ' + response.statusCode)
+      console.log(Date());
+      console.log('');
+    };
+  });
+};
 
-  function scrape(url){
-    request(url, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log('request url: ' + url);
-        console.log('response status: ' + response.statusCode)
-        console.log(Date());
-        console.log('');
-      };
-    });
-  };
-
-  for (var i = 0; i < urls.length; i++) {
-    var url = urls[i];
-    scrape(url);
-  };
-
-  console.log('');
-
-}, function () {
-  console.log('');
-  console.log('');
-  console.log('-------------------------------');
-  console.log('TEST_JOB.JS HAS STOPPED RUNNING')
-  console.log(Date());
-});
-
-job.start();
+for (var i = 0; i < urls.length; i++) {
+  var url = urls[i];
+  scrape(url);
+};
